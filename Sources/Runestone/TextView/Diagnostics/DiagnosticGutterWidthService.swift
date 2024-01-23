3fb0a1c13831f4b66fb52c0f74c39f88544d4ed3
jbromberg <58891459+jbromberg@users.noreply.github.com>
2024-01-23 11:20:22 -0500

    Update DiagnosticGutterWidthService.swift

--- Sources/Runestone/TextView/Diagnostics/DiagnosticGutterWidthService.swift
+++ Sources/Runestone/TextView/Diagnostics/DiagnosticGutterWidthService.swift
@@ -2,12 +2,12 @@ import Combine
 import UIKit
 
 final class DiagnosticGutterWidthService {
-    var gutterLeadingPadding: CGFloat = 12
+    var gutterLeadingPadding: CGFloat = 4
     var gutterTrailingPadding: CGFloat = 8
     var gutterWidth: CGFloat {
         return diagnosticWidth + gutterLeadingPadding + gutterTrailingPadding
     }
-    var diagnosticWidth: CGFloat = 36
+    var diagnosticWidth: CGFloat = 32
     let didUpdateGutterWidth = PassthroughSubject<Void, Never>()
 
     private var previouslySentGutterWidth: CGFloat?
